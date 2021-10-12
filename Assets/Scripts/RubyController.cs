a5ce588a20e1e259ed735f07ee2f9fd2e8b5e33d
Christopher <chrismikeburton@gmail.com>
2021-10-12 19:10:52 -0400

    Blocking Movement
    Tutorial Pt:6

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,23 +4,32 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     void Start()
     {
             //Framerate Adjustment
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
+            //Creates Rigidbody components
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
             //Horizontal and Vertical movement through input
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
         //Debug.Log(vertical);
         //Debug.Log(horizontal);
+        
+    }
+    private void FixedUpdate()
+    {
         Vector2 position = transform.position;
-        position.x = position.x+3.0f*horizontal*Time.deltaTime;
-        position.y = position.y+3.0f*vertical*Time.deltaTime;
-        transform.position = position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
